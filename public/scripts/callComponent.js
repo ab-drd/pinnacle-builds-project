d22cf61b276f0f573f2a43168f34d0e7f2e2a8d8
Antonia B <abartu00@fesb.hr>
2021-12-18 14:43:59 +0100

    Cleaned up unnecessary files

--- public/scripts/callComponent.js
+++ public/scripts/callComponent.js
@@ -7,7 +7,7 @@ function showCPU() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState==4 && this.status==200) {
-            
+            console.log(this.responseText);
             var arrayObject = JSON.parse(this.responseText);
 
             for (let key in arrayObject) {
