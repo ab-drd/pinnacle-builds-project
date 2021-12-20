4011cee864a9ed4367573bf81da3c0b1b2030d07
Jakov Bejo <73558068+JBejoFesb@users.noreply.github.com>
2021-12-20 23:04:25 +0100

    fixed some upload issues

--- public/src/header.php
+++ public/src/header.php
@@ -1,19 +1,24 @@
-<?php
-    session_start();
-?>
-<!DOCTYPE html>
+<!DOCTYPE html> 
 
 <html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../styles/header.css" rel="stylesheet">
+    </head>
     <body>
-        <?php
-            if (isset($_SESSION["username"])) {
-                echo `<li><a href="profile.php">Profile page</a></li>`;
-                echo `<li><a href="includes/logout.inc.php">Log out</a></li>`;
-            }
-            else {
-                echo `<li><a href="signup.php">Sign up</a></li>`;
-                echo `<li><a href="login.php">Log in</a></li>`;
-            }
-        ?>
-    </body>
+        <header>
+            <ul class="tabsLeft">
+                <li id ="logo"><a href = "../index.php"><img src = "../images/icon.png" height="40" width="40"/></a></li>
+                <li id="build"><a id=builder href="../builder.php">BUILD</a></li>
+                <li id="desktops"><a href="desktops.html">DESKTOPS</a></li>
+                <li id="laptops"><a href="laptops.html">LAPTOPS</a></li>
+                <li id="about"><a href="about.html">ABOUT US</a></li>
+            </ul>
+            <ul class="tabsRight">
+                <li id="login"><a href="login.html">LOG IN</a></li>
+                <li id="cart"><a href="viewcart.html"><img src = "../images/cart.png" height="32" width="32"/></a></li>
+            </ul>
+        </header>
+    </body>  
 </html>
+
