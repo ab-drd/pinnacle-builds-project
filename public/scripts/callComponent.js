4e818eeb334e99fe233ddcae38c8f0ad1dcc3bf0
Antonia B <abartu00@fesb.hr>
2021-12-20 23:05:43 +0100

    Login/Sign up almost working

--- public/scripts/callComponent.js
+++ public/scripts/callComponent.js
@@ -1,28 +1,59 @@
+// you're free to delete this stuff, I only added it for testing purposes
 cpu_button = document.getElementById("list-cpu");
 cpu_list = document.getElementById("listed-cpu");
 
-cpu_button.addEventListener("click", showCPU);
+cpu_button.addEventListener("click", fetchCPU);
 
-function showCPU() {
+function fetchCPU() {
+    fetchComponents("pc_case");
+}
+
+//ask backend if you want to edit this function
+function fetchComponents(component) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState==4 && this.status==200) {
-            console.log(this.responseText);
-            var arrayObject = JSON.parse(this.responseText);
-
-            for (let key in arrayObject) {
-                
-                item = arrayObject[key];
-                for (let property in arrayObject[key]){
-                    cpu_list.innerHTML += `${property} : ${arrayObject[key][property]}<br>`;
-                }
-
-                cpu_list.innerHTML += "<br>";
+        if (this.readyState == 4 && this.status == 200) {
+            try {
+                list = JSON.parse(this.responseText);
             }
-            
+            catch(err) {
+                cpu_list.innerHTML += err;
+                list = "";
+            }
+            finally {
+                componentCallBack(list, component);
+            }
         }
     }
 
-    xmlhttp.open("GET", "./src/componentRequestHandler.php?q=CPU", true);
+    xmlhttp.open("GET", "./src/componentRequestHandler.php?component=" + component, true);
     xmlhttp.send();
+}
+
+// simple render function
+function renderComponents(component_array) {
+    for (let key in component_array) {
+
+        item = component_array[key];
+
+        for (let property in item){
+            console.log(`${property}: ${item[property]}`);
+            cpu_list.innerHTML += `${property}: ${item[property]}<br>`;
+        }
+
+        cpu_list.innerHTML += "<br>";
+    }
+}
+
+function componentCallBack(list, component) {
+    // must use callback to use data recieved from the server
+    if (component == "cpu") {
+        // do something with the component list
+    }
+    else if (component == "cpu_fan") {
+        // do something else
+    }  
+    else {
+        
+    }
 }
--- public/src/includes/signup.inc.php
+++ public/src/includes/signup.inc.php
@@ -2,28 +2,28 @@
     if (isset($_POST['create'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $passwordRepeat = $_POST['passwordRepeat'];
+        $passwordRepeat = $_POST['passwordrepeat'];
 
         require_once 'function.inc.php';
         require_once 'connect.inc.php';
 
         if (invalidUsername($username) !== false) {
-            header("location: ../signup.php?error=invalidusername");
-            exit();
-        }
-
-        if (passwordLength($password) !== false) {
-            header("location: ../signup.php?error=passwordtooshort");
-            exit();
-        }
-
-        if (passwordMatch($password, $passwordRepeat) !== false) {
-            header("location: ../signup.php?error=passwordsdontmatch");
+            header("location: ../../login.php?action=signup&error=invalidusername");
             exit();
         }
 
         if (usernameExists($db_connection, $username) !== false) {
-            header("location: ../signup.php?error=usernametaken");
+            header("location: ../../login.php?action=signup&error=usernametaken");
+            exit();
+        }
+
+        if (passwordLength($password) !== false) {
+            header("location: ../../login.php?action=signup&error=passwordtooshort");
+            exit();
+        }
+
+        if (passwordMatch($password, $passwordRepeat) !== false) {
+            header("location: ../../login.php?action=signup&error=passwordsdontmatch");
             exit();
         }
 
@@ -32,7 +32,7 @@
         create_account($db_connection, $username, $passwordHash);
     }
     else {
-        header("location: ../signup.php");
+        header("location: ../../login.php?action=signup");
         exit();
     }
 ?>
