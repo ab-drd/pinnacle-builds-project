9d00841b5c021a2b8540d653253b241ae07eabe4
Antonia B <abartu00@fesb.hr>
2021-12-23 08:43:52 +0100

    Ready for sprint 1 presentation

--- public/viewcart.php
+++ public/viewcart.php
@@ -6,10 +6,13 @@
         <meta charset="utf-8">
         <title>Pinnacle Builds | Cart</title>
         <link rel="icon" href="./images/icon.png">
-        <link rel="stylesheet" href="styles/regstyle.css">
+        <link rel="stylesheet" href="styles/style.css">
     </head>
     <body>
         <?php require_once "./src/header.php"?>
+        <div class="container">
+            <p>Your cart:</p>
+        </div>
         <?php require_once "./src/footer.html" ?>
     </body>
 </html>
