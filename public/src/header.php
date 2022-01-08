6976006f2b0f8d0aaf9f4a40a30f7880c33dd3fe
Jakov Bejo <73558068+JBejoFesb@users.noreply.github.com>
2022-01-08 22:33:31 +0100

    Responsive header & footer

--- public/src/header.php
+++ public/src/header.php
@@ -11,26 +11,31 @@
     </head>
     <body>
         <header>
-            <ul class="tabsLeft">
-                <li id ="logo"><a href = "./index.php"><img src = "./images/icon.png" height="40" width="40"/></a></li>
-                <li id="build"><a id=builder href="./builder.php">BUILD</a></li>
-                <li id="desktops"><a href="desktops.html">DESKTOPS</a></li>
-                <li id="laptops"><a href="laptops.html">LAPTOPS</a></li>
-                <li id="about"><a href="about.html">ABOUT US</a></li>
-            </ul>
-            <ul class="tabsRight">
-                <?php
-                    if (isset($_SESSION["username"])) {
-                        echo '<li id="profile"><a href="./profile.php">' . $_SESSION["username"] . "'" . 's PROFILE</a></li>';
-                        echo '<li id="logout"><a href="./src/includes/logout.inc.php">LOG OUT</a></li>';
-                    }
-                    else {
-                        print '<li id="signup"><a href="./signup.php">SIGN UP</a></li>';
-                        print '<li id="login"><a href="./login.php">LOG IN</a></li>';
-                    }
-                ?>
-                <li id="cart"><a href="./viewcart.php"><img src = "./images/cart.png" height="32" width="32"/></a></li>
-            </ul>
+            <a id="logo" href="./index.php"><img src = "./images/icon.png" height="40" width="40"/></a>
+            <input type="checkbox" id="drop-toggle">
+            <div id="drop">
+                <ul class="tabsLeft">
+                    <li id="build"><a id=builder href="./builder.php">BUILD</a></li>
+                    <li id="prebuilts"><a href="prebuilts.html">PREBUILT PCS</a></li>
+                    <li id="about"><a href="about.html">ABOUT US</a></li>
+                </ul>
+                <ul class="tabsRight">
+                    <?php
+                        if (isset($_SESSION["username"])) {
+                            echo '<li id="profile"><a href="./profile.php">' . $_SESSION["username"] . "'" . 's PROFILE</a></li>';
+                            echo '<li id="logout"><a href="./src/includes/logout.inc.php">LOG OUT</a></li>';
+                        }
+                        else {
+                            print '<li id="signup"><a href="./signup.php">SIGN UP</a></li>';
+                            print '<li id="login"><a href="./login.php">LOG IN</a></li>';
+                        }
+                    ?>
+                    <li id="cart"><a href="./viewcart.php"><img src = "./images/cart.png" height="32" width="32"/></a></li>
+                </ul>
+            </div>
+            <label for="drop-toggle" id="drop-label">
+                <span></span>
+            </label>
         </header>
     </body>
 </html>
