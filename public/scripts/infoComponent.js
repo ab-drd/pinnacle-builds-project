c16520d33995cdb96a9f69e71389047a9808bd3c
Jakov Bejo <73558068+JBejoFesb@users.noreply.github.com>
2022-01-11 01:36:40 +0100

    Delete infoComponent.js

--- public/scripts/infoComponent.js
+++
@@ -1,3 +0,0 @@
-let infoTemplate = document.getElementsByClassName('infoTemplate');
-document.getElementsByClassName('infoButt').addEventListener("click", ()=>{infoTemplate.classList.add('show');});
-document.getElementsByClassName('infoClose').addEventListener("click", ()=>{infoTemplate.classList.remove('show');});
